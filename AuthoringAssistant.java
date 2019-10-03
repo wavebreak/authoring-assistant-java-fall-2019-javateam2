0ecb70b785099e025ed2a7cef4559add4eb94944
T20140338 <valerieraem@gmail.com>
2019-10-03 11:37:47 -0500

    added entering string / print string

--- AuthoringAssistant.java
+++ AuthoringAssistant.java
@@ -4,6 +4,13 @@ public class AuthoringAssistant {
    
    public static void main(String[] args) {
       /* Type your code here. */
-      
+      Scanner scanner = new Scanner(System.in);
+      String userInput;
+
+      System.out.println("Enter a sample text:");
+      userInput = scanner.nextLine();
+      System.out.println("You entered:" + userInput);
+
+
    }
 }
