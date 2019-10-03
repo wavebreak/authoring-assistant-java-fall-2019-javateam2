fe792cae77fb397630cb99d3a88b2128f7f2a60f
T20152967 <michael.keegan@tulsacc.edu>
2019-10-03 12:05:44 -0500

    Yes

--- AuthoringAssistant.java
+++ AuthoringAssistant.java
@@ -1,18 +1,36 @@
 import java.util.Scanner;
 
 public class AuthoringAssistant {
-   
+   static Scanner scanner = new Scanner(System.in);
+
+
+   public static void printMenu() {
+      System.out.println("MENU");
+      System.out.println("c - Number of non-whitespace characters");
+      System.out.println("w - Number of words");
+      System.out.println("f - Find text");
+      System.out.println("r - Replace all !'s");
+      System.out.println("s - Shorten spaces");
+      System.out.println("q - Quit");
+      System.out.println("\nChoose an option:");
+
+   }
+
+
    public static void main(String[] args) {
       /* Type your code here. */
-      Scanner scanner = new Scanner(System.in);
+
       String userInput;
+      char menuOp = 'o';
 
       System.out.println("Enter a sample text:");
       userInput = scanner.nextLine();
       System.out.println("You entered:" + userInput);
-      //ignore this comment
-      //again
 
-
+      char q;
+      do {
+         printMenu();
+      } while (menuOp != 'q');
    }
 }
+
