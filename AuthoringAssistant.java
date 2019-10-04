8cdf133a5f278603424b9104da600cfacc8a7b55
valerie <valerieraem@gmail.com>
2019-10-04 09:35:31 -0500

    i added the getNumOfWords method and added a switch statement to our do while loop to run the choices from our menu ! :)

--- AuthoringAssistant.java
+++ AuthoringAssistant.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class AuthoringAssistant {
    static Scanner scanner = new Scanner(System.in);
 
+   //counts non-whitespace characters
    public static int getNumOfNonWSCharacters(String userInput) {
       int count = 0;
 
@@ -15,8 +16,23 @@ public class AuthoringAssistant {
       return count;
    }
 
+   //counts number of words in string
+   public static int getNumOfWords(String userInput) {
+      int numWords = 0;
+
+      for(int i = 0; i < userInput.length(); i++) {
+         if(Character.isSpaceChar(userInput.charAt(i))) {
+            numWords++;
+         }
+      }
+      //this adds one more to the word count to account for the last word w/ no space after
+      numWords = numWords + 1;
+
+      return numWords;
+   }
+
    public static char printMenu() {
-       char menuOp;
+      char menuOp;
 
       System.out.println("MENU");
       System.out.println("c - Number of non-whitespace characters");
@@ -27,9 +43,9 @@ public class AuthoringAssistant {
       System.out.println("q - Quit");
       System.out.println("\nChoose an option:");
       menuOp = scanner.next().charAt(0);
+
       return menuOp;
    }
-
 
    public static void main(String[] args) {
       /* Type your code here. */
@@ -41,12 +57,29 @@ public class AuthoringAssistant {
       userInput = scanner.nextLine();
       System.out.println("You entered:" + userInput);
 
-      System.out.println(getNumOfNonWSCharacters(userInput));
+
 
       char q;
       do {
          menuOp = printMenu();
+         switch(menuOp) {
+            case 'c':
+               System.out.println(getNumOfNonWSCharacters(userInput));
+               break;
+
+            case 'w':
+               System.out.println(getNumOfWords(userInput));
+               break;
+
+            case 'f':
+               break;
+
+            case 'r':
+               break;
+
+            case 's':
+               break;
+         }
       } while (menuOp != 'q');
    }
 }
-
